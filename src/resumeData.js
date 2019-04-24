589e9b5b17d6a24a0858f89031b3f0c8fef93e4f
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2019-04-23 22:01:03 -0400

    Initial checkin

---
+++ src/resumeData.js
@@ -0,0 +1,123 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Kalpana Nagan",
+  "role": " FrontEnd Web Developer",
+  "linkedinId":"https://linkedin.com/in/kalpana-n-4b972577/",
+  "skypeid": "nkalpana5771@gmail.com",
+  "roleDescription": "living in Toronto, CA.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://linkedin.com/in/kalpana-n-4b972577/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"Creative Front-end web Developer with 3+ years of experience,leveraging latest JavaScript libraries to build responsive websites, mobile apps, and interactive features that drive business growth and improve UI, Specialized in ReactJS.",
+  "address":"North York , Toronto , Canada",
+  "website":"+1 6478632316",
+  "education":[
+    {
+      "UniversityName":"Visveswaraya Technological University",
+      "specialization":"Electonics and communication",
+      "MonthOfPassing":"Sep",
+      "YearOfPassing":"2013",
+    },
+    
+  ],
+  "work":[
+    {
+      "CompanyName":"Robert Bosch",
+      "specialization":"Senior Software Engineer",
+      "MonthOfLeaving":"",
+      "YearOfLeaving":"",
+      "Achievements":""
+    },
+    {
+      "CompanyName":"CGI",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"Mar",
+      "YearOfLeaving":"2018",
+      "Achievements":"Corona Best team award"
+    }
+  ],
+  "skillsDescription":"",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Redux"
+    },
+    {
+      "skillname":"Javascript"
+    },
+    {
+      "skillname":"Typescript"
+    },
+    {
+      "skillname":"Bootstrap"
+    },
+    {
+      "skillname":"JQuery"
+    },
+    {
+      "skillname":"Backbonejs"
+    },
+    {
+      "skillname":"Java"
+    },
+    {
+      "skillname":"SQL"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"TMI-Technician Mobile Interface",
+      "description":"Responsive webapp, Worked for this project in CGI for Bell Canada Client , This application is used by Bell field technicians across Canada.",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"TMS-Technician Mobile Schedule",
+      "description":"Mobile Application , worked fot this project in CGI for Bell Canada client, This application is used by Bell field technicians across Canada.",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
